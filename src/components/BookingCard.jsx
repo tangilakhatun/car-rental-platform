afa9fa4385b130c8f6bfa2755b3fe7a11314c02a
Tangila Khatun <ktangila6@gmail.com>
2025-11-09 20:06:38 +0600

    Initialize RentWheels project with Vite and folder structure

---
+++ src/components/BookingCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BookingCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BookingCard;
---
+++ src/pages/AddCard/AddCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AddCard;
