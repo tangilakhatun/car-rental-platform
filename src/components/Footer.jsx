9bb62e4bec13e9341dfb8543fb75e017a81d1885
Tangila Khatun <ktangila6@gmail.com>
2026-01-11 18:36:01 +0600

    add something

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -69,7 +69,7 @@ const Footer = () => {
 
   <div className="border-t border-gray-700 mt-8">
     <div className="container mx-auto px-6 py-4 flex flex-col md:flex-row justify-between items-center text-gray-400">
-      <p>© 2025 RentWheels. All Rights Reserved.</p>
+      <p>© 2026 RentWheels. All Rights Reserved.</p>
       <div className="flex space-x-4 mt-2 md:mt-0">
         <a href="#" className="hover:text-white">Terms of Service</a>
         <a href="#" className="hover:text-white">Privacy Policy</a>
